ae2defd54f559b9bdf44eac9d0a84b13c7d6e3c5
缐英达 <daegis@yeah.net>
2018-12-26 21:41:29 +0800

    almost finish

--- src/main/java/cn/aegisa/model/excelstreamerio/controller/TestController.java
+++ src/main/java/cn/aegisa/model/excelstreamerio/controller/TestController.java
@@ -1,18 +1,13 @@
 package cn.aegisa.model.excelstreamerio.controller;
 
-import cn.aegisa.model.excelstreamerio.vo.ExcelPropertyIndexModel;
-import com.alibaba.excel.ExcelWriter;
-import com.alibaba.excel.metadata.Sheet;
-import com.alibaba.excel.support.ExcelTypeEnum;
+import cn.aegisa.model.excelstreamerio.utils.ExcelStreamer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -26,40 +21,29 @@ import java.util.UUID;
 @Slf4j
 public class TestController {
 
-    @RequestMapping("/excel")
+    @RequestMapping("/test")
     @ResponseBody
     public String doTEstExcel(HttpServletResponse response) throws IOException {
-        OutputStream out = new FileOutputStream("d:/100.xlsx");
-        try {
-            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX);
-            //写第一个sheet, sheet1  数据全是List<String> 无模型映射关系
-            Sheet sheet1 = new Sheet(1, 0, ExcelPropertyIndexModel.class);
-            sheet1.setSheetName("第一个sheet");
-            for (int i = 0; i < 1000; i++) {
-                List<ExcelPropertyIndexModel> orderMainList = new ArrayList<>(1000);
-                for (int j = 0; j < 1000; j++) {
-                    ExcelPropertyIndexModel orderMain = new ExcelPropertyIndexModel();
-                    orderMain.setName(UUID.randomUUID().toString());
-                    orderMain.setAddress(UUID.randomUUID().toString());
-                    orderMain.setAge(String.valueOf((double) (j + 10)));
-                    orderMain.setEmail(UUID.randomUUID().toString());
-                    orderMain.setHeigh(UUID.randomUUID().toString());
-                    orderMain.setLast(String.valueOf(i * 1000 + j));
-                    orderMainList.add(orderMain);
+        List<String> head = new ArrayList<>();
+        head.add("name");
+        head.add("age");
+        head.add("add");
+        head.add("phone");
+        head.add("id");
+        head.add("score");
+        ExcelStreamer streamer = ExcelStreamer.getExcelStreamer("测试表单", head, "d:\\test.xlsx");
+        for (int i = 0; i < 255; i++) {
+            List<List<String>> data = new ArrayList<>();
+            for (int j = 0; j < 4096; j++) {
+                List<String> row = new ArrayList<>();
+                for (int k = 0; k < 6; k++) {
+                    row.add(UUID.randomUUID().toString());
                 }
-                System.out.println(i);
-                writer.write(orderMainList, sheet1);
+                data.add(row);
             }
-            writer.finish();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            streamer.write(data);
         }
+        streamer.finish();
         return "ok";
     }
 }
--- src/main/java/cn/aegisa/model/excelstreamerio/utils/ExcelStreamer.java
+++ src/main/java/cn/aegisa/model/excelstreamerio/utils/ExcelStreamer.java
@@ -3,7 +3,10 @@ package cn.aegisa.model.excelstreamerio.utils;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.metadata.Sheet;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.Collections;
@@ -15,37 +18,82 @@ import java.util.stream.Collectors;
  *
  * @author XIANYINGDA at 2018/12/25 16:45
  */
+@Slf4j
 public class ExcelStreamer {
 
     private ExcelWriter writer;
     private Sheet sheet;
     private boolean toFile = false;
+    private int size = 0;
+    private boolean isDone = false;
+    private OutputStream out;
 
-    private ExcelStreamer(ExcelWriter writer, Sheet sheet, boolean toFile) {
+    private ExcelStreamer(ExcelWriter writer, Sheet sheet, boolean toFile, OutputStream out) {
         this.writer = writer;
         this.sheet = sheet;
         this.toFile = toFile;
+        if (toFile) {
+            this.out = out;
+        }
     }
 
-    public void write() {
-        writer.write(Collections.EMPTY_LIST, sheet);
+    public void write(List<List<String>> dataMatrix) {
+        if (isDone) {
+            throw new RuntimeException("This excel has been done and can not be appended any more.");
+        }
+        if (dataMatrix != null && dataMatrix.size() > 0) {
+            writer.write0(dataMatrix, sheet);
+            this.size += dataMatrix.size();
+            log.info("Write {} rows, total rows: {}.", dataMatrix.size(), this.size);
+        }
     }
 
     public void finish() {
+        log.info("Committing {} rows...", size);
         writer.finish();
+        isDone = true;
+        log.info("Commit {} rows to excel.", size);
+        if (toFile) {
+            try {
+                out.close();
+            } catch (Exception e) {
+                log.error("Exception while closing the FileOutputStream", e);
+            }
+        }
     }
 
-    public static ExcelStreamer getExcelStreamer(List<String> head, OutputStream out) throws Exception {
+    private static ExcelStreamer getExcelStreamer0(List<String> head, OutputStream out, boolean toFile, String sheetName) {
         ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX);
         Sheet sheet = new Sheet(0, 1);
+        if (!StringUtils.isEmpty(sheetName)) {
+            sheet.setSheetName(sheetName);
+        }
         List<List<String>> headList = head.stream().map(Collections::singletonList).collect(Collectors.toList());
         sheet.setHead(headList);
-        return new ExcelStreamer(writer, sheet, true);
+        return new ExcelStreamer(writer, sheet, toFile, out);
     }
 
     public static ExcelStreamer getExcelStreamer(List<String> head, String filePath) throws Exception {
         OutputStream out = new FileOutputStream(filePath);
-        return getExcelStreamer(head, out);
+        return getExcelStreamer0(head, out, true, null);
+    }
+
+    public static ExcelStreamer getExcelStreamer(String sheetName, List<String> head, String filePath) {
+        OutputStream out = null;
+        try {
+            out = new FileOutputStream(filePath);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return getExcelStreamer0(head, out, true, sheetName);
+    }
+
+    public static ExcelStreamer getExcelStreamer(List<String> head, OutputStream out) throws Exception {
+        return getExcelStreamer0(head, out, false, null);
+    }
+
+    public static ExcelStreamer getExcelStreamer(String sheetName, List<String> head, OutputStream out) {
+        return getExcelStreamer0(head, out, false, sheetName);
     }
 
 }
